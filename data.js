c2c29185256e2489830607b3ba91b4e48dbd0ed1
Aleczf <71339035+Aleczf@users.noreply.github.com>
2023-11-07 11:47:02 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,22 @@
+export const menuArray = [
+    {
+        name: "Margherita",
+        ingredients: ["Pomodoro", "mozzarella", "basilico", "olio extravergine d'oliva"],
+        price: "7",
+        id: "0"
+    },
+    {
+        name: "Capricciosa",
+        ingredients: ["Pomodoro", "mozzarella", "prosciutto cotto", "carciofi", "olive"],
+        price: "12",
+        id: "1"
+    },
+    {
+        name: "Marinara",
+        ingredients: ["Pomodoro", "aglio", "acciughe", "olio extravergine d'oliva"],
+        price: "8",
+        id: "2"
+    }
+]
+
+
